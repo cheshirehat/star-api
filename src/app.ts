db9e6de8c51b3e174c1819f0747064d1faafd697
cheshirehat <cheshireinhatter@gmail.com>
2020-04-10 00:01:55 +0900

    add: エンドポイントにアクセスしたらapi叩くようにした

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import * as express from "express";
+import router from "./routes/index";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.json({ status: 200, message: "200 OK" });
 });
+
+app.use("/api", router);
 
 app.listen(3000, () => console.log("ok 3000"));
